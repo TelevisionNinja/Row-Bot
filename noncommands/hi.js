e6b71f9ce1338ffd1f66475d95267b2713a36ab3
TelevisionNinja <TelevisionNinja@gmail.com>
2020-12-09 18:39:36 -0600

    bot says hi

---
+++ noncommands/hi.js
@@ -0,0 +1,13 @@
+module.exports = {
+    description: 'Say hi',
+    args: false,
+    execute(msg) {
+        const { greetings } = require('../config.json');
+        for (let i = 0; i < greetings.length; i++) {
+            if (msg.content.toLowerCase().includes(greetings[i].toLowerCase())) {
+                msg.channel.send(greetings[Math.floor(Math.random() * (greetings.length - 1))]);
+            }
+        }
+        return true;
+    }
+}
