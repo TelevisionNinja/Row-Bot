afbace198634876f6302970815fa9b113ca28f7a
TelevisionNinja <TelevisionNinja@gmail.com>
2020-12-23 00:13:04 -0600

    choose from more posts

--- commands/rule.js
+++ commands/rule.js
@@ -11,21 +11,40 @@ module.exports = {
     usage: '<tags_w/_spaces_need_underscores>',
     cooldown: 1,
     async execute(msg, args) {
-        const url = rule + args.join('+');
+        const tags = args.join('+');
+        let url = `${rule}&tags=${tags}`;
 
         try {
-            const response = await axios.get(url);
-            const XMLStr = response.data;
+            let response = await axios.get(url);
+            let XMLStr = response.data;
+
+            let postCount = 0;
+            parseString(XMLStr, (err, result) => {
+                postCount = parseInt(result.posts['$'].count);
+            });
+
+            if (postCount === 0) {
+                msg.channel.send('Aww there\'s no results 😢');
+                return;
+            }
+
+            postCount = ~~(postCount / 100);
+            let pageMax = 2000;
+
+            if (postCount < pageMax) {
+                pageMax = postCount;
+            }
+
+            const pid = rand.randomMath(pageMax + 1);
+            url = `${url}&pid=${pid}`;
+
+            response = await axios.get(url);
+            XMLStr = response.data;
 
             let postArr = [];
             parseString(XMLStr, (err, result) => {
                 postArr = result.posts.post;
             });
-
-            if (typeof postArr === 'undefined') {
-                msg.channel.send('Aww there\'s no results 😢');
-                return;
-            }
 
             const randIndex = rand.randomMath(postArr.length);
             const img = postArr[randIndex]['$'];
