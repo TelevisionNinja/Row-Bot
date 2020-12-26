24509feb2e3578ce6b500dc212cdbfca859f773d
TelevisionNinja <TelevisionNinja@gmail.com>
2020-12-26 09:19:53 -0600

    added new year message

---
+++ generalMessages/newYear.js
@@ -0,0 +1,22 @@
+const {
+    newYear,
+} = require('../messages.json');
+
+module.exports = {
+    description: 'Wish a happy new year',
+    execute(msg) {
+        let hasReply = false;
+        let replyStr = '';
+
+        for (let i = 0; i < newYear.length; i++) {
+            if (msg === newYear[i].toLowerCase()) {
+                return {
+                    hasReply: true,
+                    replyStr: newYear[i]
+                };
+            }
+        }
+
+        return { hasReply, replyStr };
+    }
+}
