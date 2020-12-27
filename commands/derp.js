39121121e2a3e91e28dc18542f9ecb5bc10f551e
TelevisionNinja <TelevisionNinja@gmail.com>
2020-12-27 12:28:33 -0600

    separated code into functions

--- commands/derp.js
+++ commands/derp.js
@@ -2,6 +2,7 @@ const {
     derpAliases,
     derpAPIKey,
     derpAPI,
+    derpAPI50,
     derpURL
 } = require('../config.json');
 const rand = require('../lib/randomFunctions.js');
@@ -17,46 +18,102 @@ module.exports = {
     usage: '<tags separated by commas>',
     cooldown: 1,
     async execute(msg, args) {
-        const searchTerms = [...new Set(args.join('+').split(',').map(t => t.replace(/^\++|\++$/g, '')))].join('%2C');
+        const { imgURL, source, results } = await getImage(args);
 
-        if (searchTerms === '') {
-            msg.channel.send('Please provide tags');
-            return;
+        if (results) {
+            msg.channel.send(imgURL);
+            msg.channel.send(`Source: <${source}>\nResults: ${results}`);
         }
+        else {
+            msg.channel.send('Aww there\'s no results 😢');
+        }
+    }
+}
 
+async function getImage(tagArr) {
+    const searchTerms = [...new Set(tagArr.join('+').split(',').map(t => t.replace(/^\++|\++$/g, '')))].join('%2C');
+
+    //const searchTerms = Array.from(new Set(tagArr.join('+').split(',').map(t => t.replace(/^\++|\++$/g, '')))).join('%2C');
+    
+    let imgURL = '';
+    let source = '';
+    let results = 0;
+
+    if (searchTerms !== '') {
         const url = `${derpAPI}${derpAPIKey}&q=${searchTerms}`;
 
         try {
             let response = await axios.get(url);
             const count = parseInt(response.data.total);
 
-            if (!count) {
-                msg.channel.send('Aww there\'s no results 😢');
-                return;
+            if (count) {
+                const pageNum = rand.randomMath(1, count + 1);
+
+                response = await axios.get(`${url}&page=${pageNum}`);
+
+                const img = response.data.images[0];
+
+                imgURL = img.representations.full;
+                source = `${derpURL}${img.id}`;
+                results = count;
             }
-
-            // limit of 50 results per page
-            let maxPages = ~~(count / 50);
-
-            if (count % 50) {
-                maxPages++;
-            }
-
-            // page number starts at 1
-            const pageNum = rand.randomMath(1, maxPages + 1);
-
-            response = await axios.get(`${url}&page=${pageNum}&per_page=50`);
-
-            const imgArr = response.data.images;
-
-            const img = imgArr[rand.randomMath(imgArr.length)];
-
-            msg.channel.send(img.representations.full);
-            msg.channel.send(`Source: <${derpURL}${img.id}>\nResults: ${count}`);
         }
         catch (error) {
-            msg.channel.send('I couldn\'t get any results');
             console.log(error);
         }
+    }
+
+    return {
+        imgURL,
+        source,
+        results
+    }
+}
+
+async function getImage50(tagArr) {
+    const searchTerms = [...new Set(tagArr.join('+').split(',').map(t => t.replace(/^\++|\++$/g, '')))].join('%2C');
+    
+    let imgURL = '';
+    let source = '';
+    let results = 0;
+
+    if (searchTerms !== '') {
+        const url = `${derpAPI50}${derpAPIKey}&q=${searchTerms}`;
+
+        try {
+            let response = await axios.get(url);
+            const count = parseInt(response.data.total);
+
+            if (count) {
+                // limit of 50 results per page
+                let maxPages = ~~(count / 50);
+
+                if (count % 50) {
+                    maxPages++;
+                }
+
+                // page number starts at 1
+                const pageNum = rand.randomMath(1, maxPages + 1);
+
+                response = await axios.get(`${url}&page=${pageNum}&per_page=50`);
+
+                const imgArr = response.data.images;
+
+                const img = imgArr[rand.randomMath(imgArr.length)];
+
+                imgURL = img.representations.full;
+                source = `${derpURL}${img.id}`;
+                results = count;
+            }
+        }
+        catch (error) {
+            console.log(error);
+        }
+    }
+
+    return {
+        imgURL,
+        source,
+        results
     }
 }
