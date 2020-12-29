bd68670d2f50e08e5f3fafdd460a96dc60f4c828
TelevisionNinja <TelevisionNinja@gmail.com>
2020-12-29 04:39:32 -0600

    added urban command

---
+++ commands/urban.js
@@ -0,0 +1,50 @@
+const { urban } = require('../config.json');
+const rand = require('../lib/randomFunctions.js');
+const stringUtils = require('../lib/stringUtils.js');
+const querystring = require('querystring');
+const axios = require('axios');
+const Discord = require('discord.js');
+
+module.exports = {
+    names: urban.names,
+    fileName: __filename,
+    description: urban.description,
+    args: true,
+    guildOnly: false,
+    usage: '<word>',
+    cooldown: 1,
+    async execute(msg, args) {
+        const searchWord = querystring.stringify({ term: args.join(' ') });
+        let URL = `${urban.API}${searchWord}`;
+
+        const response = await axios.get(URL);
+        const defs = response.data.list;
+
+        if (defs.length) {
+            const result = defs[rand.randomMath(defs.length)];
+
+            const embed = new Discord.MessageEmbed()
+                .setTitle(result.word)
+                .setURL(result.permalink)
+                .addFields(
+                    {
+                        name: 'Definition',
+                        value: stringUtils.cutOff(result.definition, 1024)
+                    },
+                    {
+                        name: 'Example',
+                        value: stringUtils.cutOff(result.example, 1024)
+                    },
+                    {
+                        name: 'Rating',
+                        value: `👍 ${result.thumbs_up}\t👎 ${result.thumbs_down}`
+                    }
+                );
+
+            msg.channel.send(embed);
+        }
+        else {
+            msg.channel.send('Aww there\'s no results 😢');
+        }
+    }
+}
