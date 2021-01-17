d24fb2192ad3a3cd4b42c8c4f5ebe4f59b68de14
TelevisionNinja <TelevisionNinja@gmail.com>
2021-01-17 00:30:16 -0600

    send memes

---
+++ intervalMessages/memes.js
@@ -0,0 +1,33 @@
+const interval = require('../lib/interval.js');
+const msgUtils = require('../lib/msgUtils.js');
+const { memes } = require('../config.json');
+const axios = require('axios');
+const rand = require('../lib/randomFunctions.js');
+
+module.exports = {
+    description: memes.description,
+    async execute(client) {
+        const recipient = await msgUtils.getRecipient(client, memes.channelID);
+
+        interval.startIntervalFunc(
+            async () => {
+                try {
+                    const URL = memes.URLs[rand.randomMath(memes.URLs.length)];
+                    const response = await axios.get(URL);
+                    const postArr = response.data.data.children;
+
+                    const post = postArr[rand.randomMath(postArr.length)];
+
+                    recipient.send(post.data.url);
+                }
+                catch (error) {
+                    console.log(error);
+                }
+            },
+            180,
+            0,
+            0,
+            true
+        );
+    }
+}
