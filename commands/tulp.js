7927d94cc75fed28e4432d8665ce2d5c704c2a60
TelevisionNinja <TelevisionNinja@gmail.com>
2021-01-17 15:53:43 -0600

    removed variable and added check

--- commands/tulp.js
+++ commands/tulp.js
@@ -14,21 +14,19 @@ module.exports = {
     usage: '<message>',
     cooldown: 0,
     async execute(msg, args) {
-        const msgCopy = msg;
         msg.delete();
 
         if (args.length) {
-            let str = msgCopy.content.slice(prefix.length).trim();
+            let str = msg.content.slice(prefix.length).trim();
             str = str.slice(str.indexOf(' '));
 
-            const authorID = msgCopy.author.id;
-            const authorTulp = tulp.tulpsData.find(t => t.id === authorID);
+            const authorTulp = tulp.tulpsData.find(t => t.id === msg.author.id);
 
             if (typeof authorTulp === 'undefined') {
-                msgCopy.channel.send(str);
+                msg.channel.send(str);
             }
             else {
-                const guildWebhooks = await msgCopy.channel.fetchWebhooks();
+                const guildWebhooks = await msg.channel.fetchWebhooks();
                 let tulpWebhook = undefined;
 
                 for (const webhook of guildWebhooks.values()) {
@@ -40,19 +38,21 @@ module.exports = {
 
                 if (typeof tulpWebhook === 'undefined') {
                     try {
-                        tulpWebhook = await msgCopy.channel.createWebhook(authorTulp.username, {
+                        tulpWebhook = await msg.channel.createWebhook(authorTulp.username, {
                             avatar: authorTulp.avatar
                         });
                     }
                     catch (error) {
-                        msgCopy.channel.send('I couldn\'t create a webhook because there\'s too many in here 😢');
+                        msg.channel.send('I couldn\'t create a webhook because there\'s too many in here 😢');
                     }
                 }
                 else {
-                    await tulpWebhook.edit({
-                        name: authorTulp.username,
-                        avatar: authorTulp.avatar
-                    });
+                    if (tulpWebhook.name !== authorTulp.username || tulpWebhook.avatar !== authorTulp.avatar) {
+                        await tulpWebhook.edit({
+                            name: authorTulp.username,
+                            avatar: authorTulp.avatar
+                        });
+                    }
                 }
 
                 tulpWebhook.send(str);
