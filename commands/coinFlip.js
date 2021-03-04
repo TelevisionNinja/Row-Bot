b831fc9dc3411f9e2c30f0b7165f62635b9d8b47
TelevisionNinja <TelevisionNinja@gmail.com>
2021-03-04 16:47:06 -0600

    added coin flip

---
+++ commands/coinFlip.js
@@ -0,0 +1,20 @@
+const rand = require('../lib/randomFunctions.js');
+const { coin } = require('../config.json');
+
+module.exports = {
+    names: coin.names,
+    description: coin.description,
+    args: false,
+    permittedCharsOnly: false,
+    guildOnly: false,
+    usage: '',
+    cooldown: 0,
+    execute(msg, args) {
+        if (rand.randomMath(2)) {
+            msg.channel.send('Heads');
+        }
+        else {
+            msg.channel.send('Tails');
+        }
+    }
+}
