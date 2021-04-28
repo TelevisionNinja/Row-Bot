96da3fb0d77a194c969797c180b8abb62e568848
TelevisionNinja <TelevisionNinja@gmail.com>
2021-04-28 12:54:14 -0500

    removed function

--- intervalMessages/memes.js
+++ intervalMessages/memes.js
@@ -1,5 +1,4 @@
 import DailyInterval from 'daily-intervals';
-import { getRecipient } from '../lib/msgUtils.js';
 import { default as config } from '../config.json';
 import axios from 'axios';
 import { randomMath } from '../lib/randomFunctions.js';
@@ -9,8 +8,6 @@ const memes = config.memes;
 export default {
     description: memes.description,
     async execute(client) {
-        const recipient = await getRecipient(client, memes.channelID);
-
         const interval = new DailyInterval(
             async () => {
                 try {
@@ -20,7 +17,7 @@ export default {
 
                     const post = postArr[randomMath(memes.postCount)];
 
-                    recipient.createMessage(encodeURI(post.data.url));
+                    client.createMessage(memes.channelID, encodeURI(post.data.url));
                 }
                 catch (error) {
                     console.log(error);
