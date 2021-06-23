676fa5e924e3fd0950f8c946c8630ebc81e06cda
TelevisionNinja <TelevisionNinja@gmail.com>
2021-06-23 03:56:36 -0500

    source code link

---
+++ commands/sourceCode.js
@@ -0,0 +1,17 @@
+import { default as config } from '../config.json';
+
+const sourceCodeConfig = config.sourceCode;
+
+export default {
+    names: sourceCodeConfig.names,
+    description: sourceCodeConfig.description,
+    argsRequired: false,
+    argsOptional: false,
+    noSpecialChars: false,
+    guildOnly: false,
+    usage: '',
+    cooldown: 0,
+    execute(msg, args) {
+        msg.channel.createMessage(sourceCodeConfig.link);
+    }
+}
