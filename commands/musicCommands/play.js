878da7e52e520b67771e2e86e6b51e4cff2bbe48
TelevisionNinja <TelevisionNinja@gmail.com>
2021-07-05 20:38:32 -0500

    search

--- commands/musicCommands/play.js
+++ commands/musicCommands/play.js
@@ -1,5 +1,7 @@
 import { default as musicConfig } from './musicConfig.json';
 import { playYoutube } from '../../lib/audio.js';
+import { default as ytdl } from 'ytdl-core';
+import { default as ytSearch } from 'yt-search';
 
 const play = musicConfig.play;
 
@@ -9,13 +11,32 @@ export default {
     argsRequired: true,
     argsOptional: false,
     guildOnly: false,
-    usage: '<url>',
+    usage: '<url> or <search term>',
     async execute(msg, args) {
+        let songURL = '';
+
+        if (ytdl.validateURL(args[0])) {
+            songURL = args[0];
+        }
+        else {
+            const results = await ytSearch(args.join(' '));
+            const videos = results.videos;
+
+            if (videos.length) {
+                songURL = videos[0].url;
+            }
+            else {
+                msg.channel.send('No results');
+                return;
+            }
+        }
+
         if (!(msg.guild.voice && msg.guild.voice.connection)) {
             await msg.member.voice.channel.join();
         }
 
         const connection = msg.guild.voice.connection;
-        playYoutube(msg, connection, args[0]);
+
+        playYoutube(msg, connection, songURL);
     }
 }
