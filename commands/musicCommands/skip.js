8b368a29b94d5085169ce5129330dff27c0b73fd
TelevisionNinja <TelevisionNinja@gmail.com>
2021-07-08 05:03:42 -0500

    added skip

---
+++ commands/musicCommands/skip.js
@@ -0,0 +1,17 @@
+import { default as musicConfig } from './musicConfig.json';
+import { skip } from '../../lib/audio.js';
+
+const skipConfig = musicConfig.skip;
+
+export default {
+    names: skipConfig.names,
+    description: skipConfig.description,
+    argsRequired: false,
+    argsOptional: false,
+    guildOnly: false,
+    vcMemberOnly: true,
+    usage: '',
+    execute(msg, args) {
+        skip(msg);
+    }
+}
