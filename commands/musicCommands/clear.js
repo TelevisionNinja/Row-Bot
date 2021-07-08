b80918a5ecb7e318ff19d1dbea6916062110cad0
TelevisionNinja <TelevisionNinja@gmail.com>
2021-07-08 08:17:33 -0500

    added clear

---
+++ commands/musicCommands/clear.js
@@ -0,0 +1,17 @@
+import { default as musicConfig } from './musicConfig.json';
+import { clear } from '../../lib/audioQueue.js';
+
+const clearConfig = musicConfig.clear;
+
+export default {
+    names: clearConfig.names,
+    description: clearConfig.description,
+    argsRequired: false,
+    argsOptional: false,
+    guildOnly: false,
+    vcMemberOnly: true,
+    usage: '',
+    execute(msg, args) {
+        clear(msg.guild.id);
+    }
+}
