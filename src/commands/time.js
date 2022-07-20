e590d9130c1427230b3d31cb98f113fd4f5b255a
TelevisionNinja <TelevisionNinja@gmail.com>
2022-07-20 12:13:33 -0500

    changed to adding or subtracting time

--- src/commands/time.js
+++ src/commands/time.js
@@ -10,7 +10,7 @@ export default {
         options: [
             {
                 name: 'time',
-                description: 'hours:minutes',
+                description: '<hours:minutes> or <-hours:minutes>',
                 required: true,
                 type: ApplicationCommandOptionType.String
             }
@@ -22,30 +22,40 @@ export default {
     argsOptional: false,
     noSpecialChars: false,
     guildOnly: false,
-    usage: '<hours:minutes>',
+    usage: '<hours:minutes> or <-hours:minutes>',
     cooldown: 0,
     async execute(msg, args) {
-        const timeArr = args.join('').split(':').map(number => parseInt(number.trim()));
+        const timeArr = args.join('').split(':').map(number => parseInt(number, 10));
 
         if (timeArr.length !== 2 || isNaN(timeArr[0]) || isNaN(timeArr[1])) {
-            msg.reply('Enter the time in the correct 24 hour format: `hours:minutes`');
+            msg.reply('Enter the time in the correct format: `hours:minutes` or `-hours:minutes`');
         }
         else {
             const time = new Date();
-            time.setHours(...timeArr);
+
+            if (timeArr[0] < 0) {
+                timeArr[1] = -timeArr[1];
+            }
+
+            time.setHours(time.getHours() + timeArr[0], time.getMinutes() + timeArr[1]);
             msg.reply(`<t:${Math.trunc(time.valueOf() / 1000)}:t>`);
         }
     },
     async executeInteraction(interaction) {
         const timeStr = interaction.options.getString('time');
-        const timeArr = timeStr.split(':').map(number => parseInt(number.trim()));
+        const timeArr = timeStr.split(':').map(number => parseInt(number, 10));
 
         if (timeArr.length !== 2 || isNaN(timeArr[0]) || isNaN(timeArr[1])) {
-            interaction.reply('Enter the time in the correct 24 hour format: `hours:minutes`');
+            interaction.reply('Enter the time in the correct format: `hours:minutes` or `-hours:minutes`');
         }
         else {
             const time = new Date();
-            time.setHours(...timeArr);
+
+            if (timeArr[0] < 0) {
+                timeArr[1] = -timeArr[1];
+            }
+
+            time.setHours(time.getHours() + timeArr[0], time.getMinutes() + timeArr[1]);
             interaction.reply(`<t:${Math.trunc(time.valueOf() / 1000)}:t>`);
         }
     }
