7dd896093e78c357363d695d8ee2cba0faf4f758
TelevisionNinja <TelevisionNinja@gmail.com>
2023-08-17 16:46:58 -0500

    removed unused macros

--- src/lib/ai/chat.cpp
+++ src/lib/ai/chat.cpp
@@ -20,15 +20,6 @@
 #include <Windows.h>
 #endif
 
-#define ANSI_COLOR_RED     "\x1b[31m"
-#define ANSI_COLOR_GREEN   "\x1b[32m"
-#define ANSI_COLOR_YELLOW  "\x1b[33m"
-#define ANSI_COLOR_BLUE    "\x1b[34m"
-#define ANSI_COLOR_MAGENTA "\x1b[35m"
-#define ANSI_COLOR_CYAN    "\x1b[36m"
-#define ANSI_COLOR_RESET   "\x1b[0m"
-#define ANSI_BOLD          "\x1b[1m"
-
 // determine number of model parts based on the dimension
 static const std::map<int, int> LLAMA_N_PARTS = {
     { 4096, 1 },
--- src/lib/ai/readme.md
+++ src/lib/ai/readme.md
@@ -1,3 +1,3 @@
 Download the model from https://huggingface.co/Sosaka/Alpaca-native-4bit-ggml/blob/main/ggml-alpaca-7b-q4.bin
 
-Build the c++ code by running make
+Build the c++ code by using the make file
